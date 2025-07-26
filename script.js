b0649b11183e4f60292275b23e91564e3d1a5a65
Onyx McQueen <onyxmcqueen88@gmail.com>
2025-07-26 14:44:08 -0400

    SCRIPT.JS: Implemented Book constructor, addBookToLibrary function, and prototype info method

---
+++ script.js
@@ -0,0 +1,26 @@
+//Store Book Objects in an Array
+const myLibrary = [];
+
+// Constructor Function to create Book objects
+function Book(title, author, pages, isRead){
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
+
+// Put info function on Book instance prototype object.
+Book.prototype.info = function () {
+  let readStatus = this.isRead ? 'Read' : 'Not Read Yet'
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}.`;
+};
+
+// Function creates a book and then stores it in library.
+
+function addBookToLibrary(title, author, pages, isRead){
+  //Create book object
+  const book = new Book(title, author, pages, isRead);
+  //Store the book in the library array
+  myLibrary.push(book);
+}
